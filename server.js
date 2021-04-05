018367cee1116fed6226a5f8c90a73a8e49405f7
Manish Rawat <github.jmtss@gmail.com>
2021-04-05 19:59:39 +0530

    intial setup

---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+const { use } = require('./routes/tourRoutes');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE_CLOUD.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('DB connection succesfull'));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
