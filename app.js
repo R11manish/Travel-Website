845441d4f2aa3537e8c39885517c40b342d4c87c
Manish Rawat <github.jmtss@gmail.com>
2021-04-06 20:47:21 +0530

    connect database

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
   next();
 });
 
-// 3) ROUTES
+// 3) RO  UTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
-const { use } = require('./routes/tourRoutes');
 
 dotenv.config({ path: './config.env' });
 
@@ -18,6 +17,35 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => console.log('DB connection succesfull'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'please fill the name '],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A price must be there']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Himalayas',
+  rating: 5.0,
+  price: 4000
+});
+
+testTour
+  .save()
+  .then(doc => console.log(doc))
+  .catch(err => console.log(`Error 🔥🔥:${err}`));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
