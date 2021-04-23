5146530209954393928430e1459c9ce29c0d0b8a
Manish Rawat <github.jmtss@gmail.com>
2021-04-23 20:37:25 +0530

    basic error handling

--- app.js
+++ app.js
@@ -15,17 +15,35 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// 3) RO  UTES
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//error handling route
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'Fail',
+  //   message: `can't find ${req.originalUrl} on the server`
+  // });
+  const err = new Error(`can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message
+  });
+});
 
 module.exports = app;
