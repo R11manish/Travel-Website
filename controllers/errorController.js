741bb32ecf3f67ad29368554db36a3a6655d226f
Manish Rawat <github.jmtss@gmail.com>
2021-05-05 07:57:17 +0530

    fix development and production error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,38 @@
-module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
-    //console.log(err.stack);
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
 
+const sendErrorProd = (err, res) => {
+  //operational error - send to the client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-        status: err.status,
-        message: err.message
+      status: err.status,
+      message: err.message
     });
-}
+  } else {
+    // unknown errors - hided from the client
+    // 1) log on the error
+    console.error('ERROR 🔥🔥', err);
+    // 2) send generic message
+    res.status(500).json({
+      status: 'Error',
+      message: 'something went wrong!! please try again after some time'
+    });
+  }
+};
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
+};
