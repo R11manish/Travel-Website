248efbfe90d7f863c276bb54ce595e251c438f0f
Manish Rawat <github.jmtss@gmail.com>
2021-05-05 09:45:49 +0530

    fix invalid id error message

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,38 +1,46 @@
+const AppError = require('../utility/appError');
+const handleCastErrorDB = (err)=>  {
+    const message = `invalid ${err.path} : ${err.value}.`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    error: err,
-    message: err.message,
-    stack: err.stack
-  });
+    res.status(err.statusCode).json({
+        status: err.status,
+        error: err,
+        message: err.message,
+        stack: err.stack
+    });
 };
 
 const sendErrorProd = (err, res) => {
-  //operational error - send to the client
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message
-    });
-  } else {
-    // unknown errors - hided from the client
-    // 1) log on the error
-    console.error('ERROR 🔥🔥', err);
-    // 2) send generic message
-    res.status(500).json({
-      status: 'Error',
-      message: 'something went wrong!! please try again after some time'
-    });
-  }
+    //operational error - send to the client
+    if (err.isOperational) {
+        res.status(err.statusCode).json({
+            status: err.status,
+            message: err.message
+        });
+    } else {
+        // unknown errors - hided from the client
+        // 1) log on the error
+        console.error('ERROR 🔥🔥', err);
+        // 2) send generic message
+        res.status(500).json({
+            status: 'Error',
+            message: 'something went wrong!! please try again after some time'
+        });
+    }
 };
 
 module.exports = (err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
 
-  if (process.env.NODE_ENV === 'development') {
-    sendErrorDev(err, res);
-  } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
-  }
+    if (process.env.NODE_ENV === 'development') {
+        sendErrorDev(err, res);
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = { ...err };
+        if (error.name === 'CastError') error = handleCastErrorDB(error);
+        sendErrorProd(error, res);
+    }
 };
