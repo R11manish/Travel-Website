25113473b628ce4d6be2ea07f04321ee112e9e42
Manish Rawat <github.jmtss@gmail.com>
2021-05-05 10:04:06 +0530

    fix duplicate tour name error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,46 +1,53 @@
 const AppError = require('../utility/appError');
-const handleCastErrorDB = (err)=>  {
-    const message = `invalid ${err.path} : ${err.value}.`;
-    return new AppError(message, 400);
-}
+const handleCastErrorDB = err => {
+  const message = `invalid ${err.path} : ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = err => {
+  const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0];
+  const message = `Duplicate Field value :${value}.please try another one`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
-    res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack
-    });
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
 };
 
 const sendErrorProd = (err, res) => {
-    //operational error - send to the client
-    if (err.isOperational) {
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message
-        });
-    } else {
-        // unknown errors - hided from the client
-        // 1) log on the error
-        console.error('ERROR 🔥🔥', err);
-        // 2) send generic message
-        res.status(500).json({
-            status: 'Error',
-            message: 'something went wrong!! please try again after some time'
-        });
-    }
+  //operational error - send to the client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    });
+  } else {
+    // unknown errors - hided from the client
+    // 1) log on the error
+    console.error('ERROR 🔥🔥', err);
+    // 2) send generic message
+    res.status(500).json({
+      status: 'Error',
+      message: 'something went wrong!! please try again after some time'
+    });
+  }
 };
 
 module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-    if (process.env.NODE_ENV === 'development') {
-        sendErrorDev(err, res);
-    } else if (process.env.NODE_ENV === 'production') {
-        let error = { ...err };
-        if (error.name === 'CastError') error = handleCastErrorDB(error);
-        sendErrorProd(error, res);
-    }
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldDB(error);
+    sendErrorProd(error, res);
+  }
 };
