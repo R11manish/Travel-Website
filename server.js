0e904099f834b0c00fe95121df52d5103cbe4a81
Manish Rawat <github.jmtss@gmail.com>
2021-05-13 00:16:17 +0530

    fix unhandle Rejections

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ mongoose
   .then(() => console.log('DB connection succesfull'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  server.close();
+  process.exit(1);
 });
