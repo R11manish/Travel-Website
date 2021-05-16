1918568b1fb8c20f818895cbbe6cf3231ae52db8
Manish Rawat <github.jmtss@gmail.com>
2021-05-16 17:06:30 +0530

    implement jwt auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,22 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../Models/userModel');
 const catchAsync = require('../utility/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN
+  });
+
   res.status(201).json({
     status: 'Success',
+    token,
     data: {
       user: newUser
     }
