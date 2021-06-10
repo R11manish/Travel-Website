99592ee45134509535d22164e9822e3df54fa6d1
Manish Rawat <github.jmtss@gmail.com>
2021-06-10 13:12:51 +0530

    embed guides document in tour guide

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -107,7 +108,8 @@ const tourSchema = new mongoose.Schema(
       address : String,
       description : String,
       day : Number
-    }]
+    }],
+    guides : Array
   },
   {
     toJSON: { virtuals: true },
@@ -122,6 +124,12 @@ tourSchema.virtual('duration-week').get(function() {
 //document middleware - .save() , .create()
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save' , async function(next){
+  const guidesPromises = this.guides.map(async id => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 
