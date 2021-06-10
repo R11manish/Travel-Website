e13257bd343c6f38685c4ce35854059407721c85
Manish Rawat <github.jmtss@gmail.com>
2021-06-10 15:56:12 +0530

    use refer for guides in tour document

--- Models/tourModel.js
+++ Models/tourModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 const validator = require('validator');
-const User = require('./userModel');
+// const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -109,7 +109,11 @@ const tourSchema = new mongoose.Schema(
       description : String,
       day : Number
     }],
-    guides : Array
+    guides : [
+      {type : mongoose.Schema.ObjectId,
+       ref : 'User'
+      }
+    ]
   },
   {
     toJSON: { virtuals: true },
@@ -127,11 +131,14 @@ tourSchema.pre('save', function(next) {
   next();
 });
 
-tourSchema.pre('save' , async function(next){
-  const guidesPromises = this.guides.map(async id => await User.findById(id));
-  this.guides = await Promise.all(guidesPromises);
-  next();
-});
+// works by embeding document
+// tourSchema.pre('save' , async function(next){
+//   const guidesPromises = this.guides.map(async id => await User.findById(id));
+//   this.guides = await Promise.all(guidesPromises);
+//   next();
+// });
+
+
 
 //Query Middleware
 tourSchema.pre(/^find/, function(next) {
