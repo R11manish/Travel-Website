b5e28da79fd402a0ba8498097323419bea1e4f5e
Manish Rawat <github.jmtss@gmail.com>
2021-06-11 16:24:08 +0530

    populate review with user and tour specific data

--- Models/reviewModel.js
+++ Models/reviewModel.js
@@ -39,13 +39,16 @@ const reviewSchema = new mongoose.Schema({
 });
 
 //Query Midddleware
-// tourSchema.pre(/^find/ , function(next){
-//     this.populate({
-//       path : 'tour',
-//       select : '-__v '
-//     });
-//     next();
-//   });
+reviewSchema.pre(/^find/ , function(next){
+    this.populate({
+      path : 'tour',
+      select : 'name'
+    }).populate({
+        path : 'user',
+        select : 'name'
+      });
+    next();
+  });
 
 const Review = mongoose.model('Review', reviewSchema);
 
