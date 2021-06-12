e8b70f43369f8950489ed7c7827fe2a4eda24ae5
Manish Rawat <github.jmtss@gmail.com>
2021-06-13 00:01:44 +0530

    added real world data in database

--- dev-data/data/script.js
+++ dev-data/data/script.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 const Tour = require('../../Models/tourModel');
+const User = require('../../Models/userModel');
+const Review = require('../../Models/reviewModel');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -20,10 +22,14 @@ mongoose
   .then(() => console.log('DB connection succesfull'));
 
 const tours = JSON.parse(fs.readFileSync('./tours.json', 'utf-8'));
+const users = JSON.parse(fs.readFileSync('./users.json', 'utf-8'));
+const reviews = JSON.parse(fs.readFileSync('./reviews.json', 'utf-8'));
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('successfully added data into database');
   } catch (err) {
     console.log(err);
@@ -34,6 +40,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('deleted data Successfully');
   } catch (error) {
     console.log(error);
