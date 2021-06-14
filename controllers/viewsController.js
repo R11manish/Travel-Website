7e1d518ca2b599a0653ce840b3b49b34baee37bd
Manish Rawat <github.jmtss@gmail.com>
2021-06-14 17:42:11 +0530

    enchanced overview page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,13 +1,19 @@
+const Tour = require('../Models/tourModel');
+const catchAsync = require('../utility/catchAsync');
 
-exports.getOverview = (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
+
+    const tours = await Tour.find()
+
     res.status(200).render('overview', {
-        title: 'All tour'
-    })
-}
+        title: 'All tour',
+        tours
+    });
+});
 
 
 
-exports.tour = (req, res) => {
+exports.getTour = (req, res) => {
     res.status(200).render('tour', {
         title: 'The Forest Hiker Tour'
     })
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,6 +5,6 @@ const viewsController = require('../controllers/viewsController')
 const router = express.Router();
 
 router.get('/', viewsController.getOverview);
-router.get('/tour', viewsController.tour);
+router.get('/tour', viewsController.getTour);
 
 module.exports = router;
