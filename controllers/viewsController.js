b5c457498a1a692ffce4a0011e9d0f762c53ba52
Manish Rawat <github.jmtss@gmail.com>
2021-06-16 09:40:19 +0530

    setup login page

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,15 +13,22 @@ exports.getOverview = catchAsync(async (req, res, next) => {
 
 
 
-exports.getTour = catchAsync(async (req, res , next) => {
+exports.getTour = catchAsync(async (req, res, next) => {
 
     const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-        path : 'reviews',
-        field : 'review rating user'
+        path: 'reviews',
+        field: 'review rating user'
     });
 
     res.status(200).render('tour', {
-        title: `${tour.name} Tour` ,
+        title: `${tour.name} Tour`,
         tour
     })
 });
+
+
+exports.getLoginForm = async (req, res) => {
+    res.status(200).render('login', {
+        title: 'Log in to your account'
+    });
+}
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,4 +8,6 @@ router.get('/', viewsController.getOverview);
 
 router.get('/tour/:slug', viewsController.getTour);
 
+router.get('/login', viewsController.getLoginForm);
+
 module.exports = router;
