a1b28560487da8963ae9bd6ed9f0b824a044f3ec
Manish Rawat <github.jmtss@gmail.com>
2021-06-22 19:00:30 +0530

    created a newbooking on checkout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../Models/tourModel');
+const Booking = require('../Models/bookingModel')
 const catchAsync = require('../utility/catchAsync');
 const AppError = require('../utility/appError');
 const factory = require('./handlerFactory');
@@ -11,7 +12,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //2 ) create checkout session
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
-        success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
+        success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
@@ -33,4 +34,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         status: 'success',
         session
     })
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+    //this is for just temporaty because it is unsecure anyone can make purchase tour without even making 
+    const { tour, user, price } = req.query;
+    if (!tour && !user && !price) return next();
+    Booking.create({ tour, user, price });
+    res.redirect(req.originalUrl.split('?')[0])
 });
