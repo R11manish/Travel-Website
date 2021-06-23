e9a361ed0800000afb3cb07db164ab2651820da8
Manish Rawat <github.jmtss@gmail.com>
2021-06-23 08:06:48 +0530

    fix bugs on booking routes and added other operation in this route

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -43,3 +43,10 @@ exports.createBookingCheckout = catchAsync(async (req, res, next) => {
     Booking.create({ tour, user, price });
     res.redirect(req.originalUrl.split('?')[0])
 });
+
+
+exports.createBooking = factory.createOne(Booking);
+exports.getBooking = factory.getOne(Booking);
+exports.getAllBooking = factory.getAll(Booking);
+exports.updateBooking = factory.updateOne(Booking);
+exports.deleteBooking = factory.deleteOne(Booking);
--- routes/bookingRoute.js
+++ routes/bookingRoute.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const bookingController = require('./../controllers/bookingController');
-const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController')
 
 
 const router = express.Router();
 
-router.get('/checkout-session/:tourId', authController.protect, bookingController.getCheckoutSession)
+router.use(authController.protect)
+
+router.get('/checkout-session/:tourId', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'))
+
+router.get('/', bookingController.getAllBooking).post(bookingController.createBooking);
+
+router
+    .route('/:id')
+    .get(bookingController.getBooking)
+    .patch(bookingController.updateBooking)
+    .delete(bookingController.deleteBooking)
 
 module.exports = router;
