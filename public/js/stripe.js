44c376cc24e4b7a2d91b413ca2eb259d9caadde2
Manish Rawat <github.jmtss@gmail.com>
2021-06-23 16:39:10 +0530

    done some compression and optimization

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,11 +6,11 @@ export const bookTour = async tourId => {
 
     try {
         //1 ) get chekout session from API
-        const session = await axios(`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`)
+        const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`)
         // console.log(session);
         //2 ) create checkout form + charge the credit card
         await stripe.redirectToCheckout({
-            sessionId : session.data.session.id
+            sessionId: session.data.session.id
         })
     } catch (err) {
         showAlert('error', err);
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -4,7 +4,7 @@ import { showAlert } from "./alerts";
 export const updateSettings = async (data, type) => {
     try {
 
-        const url = type === 'password' ? 'http://127.0.0.1:3000/api/v1/users/update-password' : 'http://127.0.0.1:3000/api/v1/users/update-me'
+        const url = type === 'password' ? '/api/v1/users/update-password' : '/api/v1/users/update-me'
         const res = await axios({
             method: 'PATCH',
             url,
